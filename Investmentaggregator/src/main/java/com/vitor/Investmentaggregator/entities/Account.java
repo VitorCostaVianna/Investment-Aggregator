ad5592ecc328c94d935c4bb01c5b27ede116fcff
VitorCostaVianna <vcvianna@sga.pucmminas.br>
2025-01-13 14:35:37 -0300

    create entity Account

---
+++ Investmentaggregator/src/main/java/com/vitor/Investmentaggregator/entities/Account.java
@@ -0,0 +1,51 @@
+package com.vitor.Investmentaggregator.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = Account.TABLE_NAME)
+public class Account {
+    
+    private static final String TABLE_NAME = "tb_accounts";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+
+    public Account() {
+    }
+
+    public Account(UUID accountId, String description) {
+        this.setAccountId(accountId);
+        this.setDescription(description);
+    }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(UUID accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
